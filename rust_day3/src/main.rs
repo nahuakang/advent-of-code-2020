40bf03b11478c151f340021e7f1cc541b159d0eb
nahuakang <kangnahua@gmail.com>
2020-12-03 13:38:22 +0100

    Copy redditor aurele's Rust style; update readme

--- rust_day3/src/main.rs
+++ rust_day3/src/main.rs
@@ -9,34 +9,23 @@ fn main() {
         [1, 2],
     ];
 
-    let input = read_to_string("./src/input.txt").expect("cannot read file");
-    let local_map: Vec<&str> = input.split('\n').collect();
+    let local_map = read_to_string("./src/input.txt").expect("cannot read file");
+    // let local_map: Vec<&str> = input.split('\n').collect();
     
     let part_one_count = count_trees(&local_map, 3, 1);
     println!("Part one answer is: {}", part_one_count);
 
-    let mut part_two_product = 1i64;
-    for slope in SLOPES.iter() {
-        let (x_slope, y_slope) = (slope[0], slope[1]);
-        let new = count_trees(&local_map, x_slope, y_slope) as i64;
-        part_two_product = part_two_product * new;
-    }
+    let part_two_product: i64 = SLOPES
+        .iter()
+        .map(|slopes| count_trees(&local_map, slopes[0], slopes[1]) as i64)
+        .product();
     println!("Part two answer is: {}", part_two_product);
 }
 
-fn count_trees(local_map: &Vec<&str>, x_slope: usize, y_slope: usize) -> i32 {
-    let (mut tree_count, mut x_pos, mut y_pos) = (0i32, 0usize, 0usize);
-    let map_width = local_map[0].chars().count();
-    
-    while y_pos < local_map.len() {
-        let current_pos = local_map[y_pos as usize].chars().nth(x_pos as usize).unwrap();
-        if current_pos == '#' {
-            tree_count += 1
-        }
-
-        x_pos = (x_pos + x_slope) % map_width;
-        y_pos = y_pos + y_slope;
-    }
-
-    tree_count
+fn count_trees(local_map: &str, x_slope: usize, y_slope: usize) -> i32 {
+    local_map.lines()
+        .step_by(y_slope)
+        .enumerate()
+        .filter(|(step, line)| line.chars().nth(step * x_slope % line.len()).unwrap() == '#')
+        .count() as i32
 }
