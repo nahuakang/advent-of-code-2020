81f2b6152c2d6af40f860e97c319bee990998245
nahuakang <kangnahua@gmail.com>
2020-12-07 20:43:43 +0100

    Simplify types in current solution

--- rust_day7/src/main.rs
+++ rust_day7/src/main.rs
@@ -2,18 +2,21 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
+type Rule<'a> = Vec<&'a str>;
+type Rules<'a> = HashMap<&'a str, Rule<'a>>;
+
 fn main() {
     let luggage_rules = read_to_string("./src/input.txt").expect("cannot read from file");
     let luggage_rules: &str = luggage_rules.trim();
 
-    let rules: HashMap<&str, Vec<&str>> = get_bag_rules(luggage_rules);
+    let rules: Rules = get_bag_rules(luggage_rules);
 
     let count = count_bags(&rules, "shiny gold");
     println!("Part 1 count: {}", count);
 }
 
-fn get_bag_rules(raw_input: &str) -> HashMap<&str, Vec<&str>> {
-    let mut rules: HashMap<&str, Vec<&str>> = HashMap::new();
+fn get_bag_rules(raw_input: &str) -> Rules {
+    let mut rules: Rules = HashMap::new();
     let re = Regex::new(r"^(\w+\s\w+)\sbags|\d\s(\w+\s\w+)\sbags*").unwrap();
 
     for line in raw_input.lines() {
@@ -27,7 +30,7 @@ fn get_bag_rules(raw_input: &str) -> HashMap<&str, Vec<&str>> {
         
         let bag = list[0];
         if !(bag == "shiny gold") {
-            let rule: Vec<&str> = list[1..].to_vec();
+            let rule: Rule = list[1..].to_vec();
             rules.insert(bag, rule);
         }
     }
@@ -35,7 +38,7 @@ fn get_bag_rules(raw_input: &str) -> HashMap<&str, Vec<&str>> {
     rules
 }
 
-fn count_bags<'a>(bag_rules: &HashMap<&'a str, Vec<&'a str>>, target: &'a str) -> usize {
+fn count_bags(bag_rules: &Rules, target: & str) -> usize {
     let mut count = 0;
     for (_, rules) in bag_rules.iter() {
         if contains_bag(bag_rules, rules, target) {
@@ -46,7 +49,7 @@ fn count_bags<'a>(bag_rules: &HashMap<&'a str, Vec<&'a str>>, target: &'a str) -
     count
 }
 
-fn contains_bag<'a>(bag_rules: &HashMap<&'a str, Vec<&'a str>>, rule: &Vec<&'a str>, target: &'a str) -> bool {
+fn contains_bag(bag_rules: &Rules, rule:&Rule, target: &str) -> bool {
     for &bag in rule.iter() {
         if bag == target || contains_bag(bag_rules, bag_rules.get(bag).unwrap(), target) {
             return true;
