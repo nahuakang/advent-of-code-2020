d71ad6cd7f127df1d0a3127ab0c0473106156816
nahuakang <kangnahua@gmail.com>
2020-12-08 11:26:46 +0100

    Complete day 8 part 1; ugly code needs refactoring

---
+++ rust_day8/src/main.rs
@@ -0,0 +1,77 @@
+use std::fs::read_to_string;
+
+type Instruction<'a> = (&'a str, &'a str, i32);
+type Instructions<'a> = Vec<Instruction<'a>>;
+
+fn main() {
+    let raw = read_to_string("./src/input.txt").expect("cannot read file");
+    let raw = raw.trim();
+    let instructions = instructions_to_vec(raw);
+    
+    let part_1_acc = infinite_loop(&instructions);
+    println!("{}", part_1_acc);
+
+    // let part_2_acc = terminate_loop(&instructions);
+    // println!("{}", part_2_acc);
+}
+
+fn instructions_to_vec(instructions: &str) -> Instructions {
+    let mut ret = Vec::new();
+    for instruction in instructions.lines() {
+        let temp: Vec<&str> = instruction.split(" ").collect();
+        let (command, info) = (temp[0], temp[1]);
+        let (sign, digit) = info.split_at(1);
+        ret.push(
+            (command, sign, digit.parse::<i32>().unwrap()),
+        )
+    }
+    ret
+}
+
+fn infinite_loop(instructions: &Instructions) -> i32 {
+    let (mut pos, mut acc) = (0i32, 0i32);
+    let mut has_been: Vec<i32> = Vec::new();
+
+    loop {
+        if has_been.contains(&pos) {
+            break;
+        }
+        has_been.push(pos);
+        println!("has seen: {:?}", has_been);
+        let new_result = update_instruction(&instructions[pos as usize], pos, acc);
+        println!();
+        pos = new_result.0;
+        acc = new_result.1;
+    }
+    
+    acc
+}
+
+fn update_instruction(instruction: &Instruction, curr_pos: i32, acc: i32) -> (i32, i32) {
+    let mut new_acc = 0i32;
+    let new_pos: i32;
+    
+    match instruction.0 {
+        "acc" => {
+            match instruction.1 {
+                "+" => new_acc = acc + instruction.2,
+                _ => new_acc = acc - instruction.2,
+            }
+            new_pos = curr_pos + 1;
+        }
+        "jmp" => {
+            match instruction.1 {
+                "+" => new_pos = curr_pos + instruction.2,
+                _ => new_pos = curr_pos - instruction.2,
+            }
+            new_acc = acc;
+        }
+        _ => {
+            new_pos = curr_pos + 1;
+            new_acc = acc;
+        }
+    }
+    println!("instruct {}, sign {}, value {}", instruction.0, instruction.1, instruction.2);
+    println!("new pos {}, new acc {}", new_pos, new_acc);
+    (new_pos, new_acc)
+}
