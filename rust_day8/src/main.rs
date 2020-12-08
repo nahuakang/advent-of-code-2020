adf3cf4fcf921226480ecffc35f340874999224b
nahuakang <kangnahua@gmail.com>
2020-12-08 18:49:23 +0100

    Solve day 8 part 2 in Rust (might need refactoring)

--- rust_day8/src/main.rs
+++ rust_day8/src/main.rs
@@ -7,13 +7,12 @@ fn main() {
     let raw = read_to_string("./src/input.txt").expect("cannot read file");
     let raw = raw.trim();
     let instructions = instructions_to_vec(raw);
-    println!("{:?}", instructions);
     
-    let part_1_acc = infinite_loop(&instructions);
+    let (part_1_acc, _) = infinite_loop(&instructions, Vec::new(), 0, 0);
     println!("{}", part_1_acc);
 
-    // let part_2_acc = terminate_loop(&instructions);
-    // println!("{}", part_2_acc);
+    let part_2_acc = find_finite_loop(&instructions);
+    println!("{}", part_2_acc);
 }
 
 fn instructions_to_vec(instructions: &str) -> Instructions {
@@ -35,27 +34,29 @@ fn instructions_to_vec(instructions: &str) -> Instructions {
     ret
 }
 
-fn infinite_loop(instructions: &Instructions) -> i32 {
-    let (mut pos, mut acc) = (0i32, 0i32);
-    let mut visited: Vec<i32> = Vec::new();
+fn infinite_loop(instructions: &Instructions, mut visited: Vec<i32>, mut pos: i32, mut acc: i32) -> (i32, bool) {
+    let mut finite = true;
 
     loop {
-        if visited.contains(&pos) {
+        if pos as usize > instructions.len() - 1 {
             break;
         }
-        visited.push(pos);
-        println!("visited: {:?}", visited);
-        println!();
-        update_instruction(&instructions[pos as usize], &mut pos, &mut acc);
+
+        if visited.contains(&pos) {
+            finite = false;
+            break;
+        }
+
+        if let Some(instruction) = &instructions.get(pos as usize) {
+            visited.push(pos);
+            update_instruction(instruction, &mut pos, &mut acc);
+        }
     }
     
-    acc
+    (acc, finite)
 }
 
 fn update_instruction(instruction: &Instruction, curr_pos: &mut i32, acc: &mut i32) {
-    // let mut new_acc = 0i32;
-    // let new_pos: i32;
-    
     match instruction.0 {
         "acc" => {
             *acc += instruction.1;
@@ -68,6 +69,51 @@ fn update_instruction(instruction: &Instruction, curr_pos: &mut i32, acc: &mut i
             *curr_pos += 1;
         }
     }
-    println!("instruct {}, value {}", instruction.0, instruction.1);
-    println!("new pos {}, new acc {}", *curr_pos, *acc);
+}
+
+fn find_finite_loop(instructions: &Instructions) -> i32 {
+    let mut pos = 0i32;
+    let mut acc = 0i32;
+    let mut visited: Vec<i32> = Vec::new();
+
+    loop {
+        visited.push(pos);
+        let instruction = instructions[pos as usize];
+        
+        if should_try_swap_instruction(&instruction) {
+            let (mut temp_pos, mut temp_acc) = (pos, acc);
+            swap_and_update_instruction(&instruction, &mut temp_pos, &mut temp_acc);
+
+            let (inner_acc, finite) = infinite_loop(&instructions, visited.clone(), temp_pos, temp_acc);
+
+            match finite {
+                true => {
+                    acc = inner_acc;
+                    break;
+                },
+                false => {
+                    update_instruction(&instruction, &mut pos, &mut acc);
+                },
+            }
+        } else {
+            update_instruction(&instruction, &mut pos, &mut acc);
+        }
+    }
+    
+    acc
+}
+
+fn should_try_swap_instruction(instruction: &Instruction) -> bool {
+    match instruction.0 {
+        "jmp" | "nop" => true,
+        _ => false,
+    }
+}
+
+fn swap_and_update_instruction(instruction: &Instruction, curr_pos: &mut i32, acc: &mut i32) {
+    match instruction.0 {
+        "jmp" => update_instruction(&("nop", instruction.1), curr_pos, acc),
+        "nop" => update_instruction(&("jmp", instruction.1), curr_pos, acc),
+        _ => panic!(),
+    }
 }
