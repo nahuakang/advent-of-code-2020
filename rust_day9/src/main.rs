757e9127dafca53fa6cb00f889475b8448d4444a
nahuakang <kangnahua@gmail.com>
2020-12-09 21:19:18 +0100

    Try using itertools crate for combinations

--- rust_day9/src/main.rs
+++ rust_day9/src/main.rs
@@ -1,11 +1,11 @@
-use std::collections::HashSet;
+use itertools::Itertools;
 use std::fs::read_to_string;
 
 fn main() {
     let input = read_from_file("./src/input.txt");
-    let part_one_num = part_one(&input, 25).unwrap();
+    let part_one_num = find_invalid_number(&input, 25).unwrap();
     println!("Part one solution: {}", part_one_num);
-    let part_two_num = part_two(&input, 25).unwrap();
+    let part_two_num = find_weakness(&input, 25).unwrap();
     println!("Part two solution: {}", part_two_num);
 }
 
@@ -19,41 +19,26 @@ fn read_from_file(path: &str) -> Vec<i64> {
         .collect()
 }
 
-fn has_valid_sum(prev_nums: &[i64], target: i64) -> bool {
-    let mut visited = HashSet::<i64>::new();
-    for &num in prev_nums.iter() {
-        let need = target - num;
-        if visited.contains(&need) {
-            return true;
-        }
-        visited.insert(num);
-    }
-
-    false
+fn find_invalid_number(input: &Vec<i64>, preamble_size: usize) -> Option<i64> {
+    input
+        .windows(preamble_size + 1)
+        .find(|window| {
+            window[..preamble_size]
+                .iter()
+                .combinations(2)
+                .all(|combo| window[preamble_size] != combo.iter().copied().sum())
+        })
+        .map(|window| window[preamble_size])
 }
 
-fn part_one(input: &Vec<i64>, preamble_size: usize) -> Option<i64> {
-    for window in input.windows(preamble_size + 1) {
-        let target = *window.last().unwrap();
-        let prev_nums = &window[..preamble_size];
-        if !has_valid_sum(prev_nums, target) {
-            return Some(target);
-        }
-    }
-
-    None
-}
-
-fn part_two(input: &Vec<i64>, preamble_size: usize) -> Option<i64> {
-    let part_one_sum = part_one(&input, preamble_size).unwrap();
-    for window_size in 2..=input.len() {
-        for window in input.windows(window_size) {
-            let curr_sum: i64 = window.iter().sum();
-            if curr_sum == part_one_sum {
-                return Some(
-                    window.iter().min().unwrap() + window.iter().max().unwrap()
-                );
-            }
+fn find_weakness(input: &Vec<i64>, preamble_size: usize) -> Option<i64> {
+    let invalid_number = find_invalid_number(input, preamble_size).unwrap();
+    for size in 2..input.len() {
+        if let Some(window) = input
+            .windows(size)
+            .find(|&window| invalid_number == window.iter().sum())
+        {
+            return Some(window.iter().max().unwrap() + window.iter().min().unwrap());
         }
     }
 
@@ -62,17 +47,17 @@ fn part_two(input: &Vec<i64>, preamble_size: usize) -> Option<i64> {
 
 #[cfg(test)]
 mod tests {
-    use super::{read_from_file, part_one, part_two};
+    use super::{find_invalid_number, find_weakness, read_from_file};
 
     #[test]
     fn test_part_one() {
         let input = read_from_file("./src/test_input.txt");
-        assert_eq!(part_one(&input, 5).unwrap(), 127);
+        assert_eq!(find_invalid_number(&input, 5).unwrap(), 127);
     }
 
     #[test]
     fn test_part_two() {
         let input = read_from_file("./src/test_input.txt");
-        assert_eq!(part_two(&input, 5).unwrap(), 62);
+        assert_eq!(find_weakness(&input, 5).unwrap(), 62);
     }
 }
