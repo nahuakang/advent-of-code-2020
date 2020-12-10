5a5b75229fc1933f5186e7b9511d48d428897fa0
nahuakang <kangnahua@gmail.com>
2020-12-10 17:29:24 +0100

    Further refactoring on day 10 solution

--- rust_day10/src/main.rs
+++ rust_day10/src/main.rs
@@ -12,23 +12,27 @@ fn main() {
 fn load_adapters(path: &str) -> Vec<usize> {
     let input = read_to_string(path).expect("cannot parse file");
 
-    input
+    let mut adapters: Vec<usize> = input
         .trim()
         .lines()
         .map(|num| num.parse::<usize>().unwrap())
-        .collect()
+        .collect();
+    
+    adapters.sort();
+    adapters.insert(0, 0);
+    adapters.insert(adapters.len(), adapters.last().unwrap() + 3);
+
+    adapters
 }
 
 fn chain_and_find_diffs(adapters: &mut Vec<usize>) -> HashMap<usize, usize> {
     let mut diff_counter = HashMap::new();
-    adapters.sort();
-    adapters.insert(0, 0);
-    adapters.insert(adapters.len(), adapters.last().unwrap() + 3);
-    for pair in adapters.windows(2) {
+
+    adapters.windows(2).for_each(|pair| {
         let diff = pair[1] - pair[0];
         let counter = diff_counter.entry(diff).or_insert(0);
         *counter += 1;
-    }
+    });
 
     diff_counter
 }
@@ -36,14 +40,16 @@ fn chain_and_find_diffs(adapters: &mut Vec<usize>) -> HashMap<usize, usize> {
 fn part_one(adapters: &Vec<usize>) -> usize {
     let mut adapters: Vec<usize> = adapters.iter().copied().collect();
     let diff_counter = chain_and_find_diffs(&mut adapters);
+    
     diff_counter[&1] * diff_counter[&3]
 }
 
 fn count_routes(adapters: &Vec<usize>) -> HashMap<isize, usize> {
     let mut routes = HashMap::<isize, usize>::new();
-    
     routes.insert(adapters[0] as isize, 1);
     
+    // Each joltage route is equal to the sum of the number of routes to the previous three joltages.
+    // For joltages that are not present in the list of adaptors, provide 0 as value.
     for &adapter in adapters[1..].iter() {
         let mut count = 0;
         for i in (adapter as isize - 3isize)..(adapter as isize) {
@@ -60,14 +66,11 @@ fn count_routes(adapters: &Vec<usize>) -> HashMap<isize, usize> {
 }
 
 fn part_two(adapters: &Vec<usize>) -> usize {
-    let mut adapters: Vec<usize> = adapters.iter().copied().collect();
-    adapters.sort();
-    adapters.insert(0, 0);
     *count_routes(&adapters).values().max().unwrap()
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::{load_adapters, part_one, part_two};
 
     #[test]
