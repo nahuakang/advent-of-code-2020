2057ac21931c9a627d8e283d106fadc427ed25ec
nahuakang <kangnahua@gmail.com>
2020-12-10 16:40:58 +0100

    Refactor input mutability; add test 2

--- rust_day10/src/main.rs
+++ rust_day10/src/main.rs
@@ -2,10 +2,9 @@ use std::collections::HashMap;
 use std::fs::read_to_string;
 
 fn main() {
-    let mut adapters = load_adapters("./src/input.txt");
-    let part_one_answer = part_one(&mut adapters);
+    let adapters = load_adapters("./src/input.txt");
+    let part_one_answer = part_one(&adapters);
     println!("Part one answer: {}", part_one_answer);
-    println!("adapters: {:?}", adapters);
     let part_two_answer = part_two(&adapters);
     println!("Part two answer: {}", part_two_answer);
 }
@@ -23,7 +22,6 @@ fn load_adapters(path: &str) -> Vec<usize> {
 fn chain_and_find_diffs(adapters: &mut Vec<usize>) -> HashMap<usize, usize> {
     let mut diff_counter = HashMap::new();
     adapters.sort();
-    println!("adapters: {:?}", adapters);
     adapters.insert(0, 0);
     adapters.insert(adapters.len(), adapters.last().unwrap() + 3);
     for pair in adapters.windows(2) {
@@ -35,37 +33,37 @@ fn chain_and_find_diffs(adapters: &mut Vec<usize>) -> HashMap<usize, usize> {
     diff_counter
 }
 
-fn part_one(adapters: &mut Vec<usize>) -> usize {
-    let diff_counter = chain_and_find_diffs(adapters);
-    println!("diff counter: {:?}", diff_counter);
+fn part_one(adapters: &Vec<usize>) -> usize {
+    let mut adapters: Vec<usize> = adapters.iter().copied().collect();
+    let diff_counter = chain_and_find_diffs(&mut adapters);
     diff_counter[&1] * diff_counter[&3]
 }
 
 fn count_routes(adapters: &Vec<usize>) -> HashMap<isize, usize> {
     let mut routes = HashMap::<isize, usize>::new();
     
-    routes.insert(0, 1);
+    routes.insert(adapters[0] as isize, 1);
     
     for &adapter in adapters[1..].iter() {
         let mut count = 0;
-        println!("adapter is {}", adapter);
         for i in (adapter as isize - 3isize)..(adapter as isize) {
             let num = match routes.get(&i) {
                 Some(&num) => num,
                 None => 0,
             };
-            println!("count is {}, num is {}", count, num);
             count += num;
         }
         routes.insert(adapter as isize, count);
     }
-    println!("routes: {:?}", routes);
 
     routes
 }
 
 fn part_two(adapters: &Vec<usize>) -> usize {
-    *count_routes(adapters).values().max().unwrap()
+    let mut adapters: Vec<usize> = adapters.iter().copied().collect();
+    adapters.sort();
+    adapters.insert(0, 0);
+    *count_routes(&adapters).values().max().unwrap()
 }
 
 #[cfg(test)]
@@ -76,11 +74,15 @@ mod tests{
     fn test_part_one() {
         let mut input = load_adapters("./src/test_input.txt");
         assert_eq!(part_one(&mut input), 35);
+        let mut input2 = load_adapters("./src/test_input2.txt");
+        assert_eq!(part_one(&mut input2), 220);
     }
 
     #[test]
     fn test_part_two() {
         let input = load_adapters("./src/test_input.txt");
         assert_eq!(part_two(&input), 8);
+        let input2 = load_adapters("./src/test_input2.txt");
+        assert_eq!(part_two(&input2), 19208);
     }
 }
