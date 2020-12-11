fcfec731913b7f482ed4cba5d5aab50b13d797ab
nahuakang <kangnahua@gmail.com>
2020-12-11 22:31:16 +0100

    Solve part 2 of day 11; ugly code

--- rust_day11/src/main.rs
+++ rust_day11/src/main.rs
@@ -6,7 +6,10 @@ fn main() {
     let raw_input =  raw_input.trim();
     let mut waiting_area = Seats::new(raw_input);
     waiting_area.allocate_seats_until_stabilization();
-    println!("Part one answer: {}", waiting_area.get_occupied_count());
+    println!("Part one answer: {}", waiting_area.count_occupied_seats());
+    let mut waiting_area = Seats::new(raw_input);
+    waiting_area.allocate_seats_until_stabilization_2();
+    println!("Part two answer: {}", waiting_area.count_occupied_seats());
 }
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
@@ -30,11 +33,15 @@ type Seat = (usize, usize);
 #[derive(Debug)]
 struct Seats {
     seats: HashMap<Seat, Status>,
+    rows: usize,
+    cols: usize,
 }
 
 impl Seats {
     fn new(raw_seats_input: &str) -> Self {
         let mut seats = HashMap::<Seat, Status>::new();
+        let rows: usize = raw_seats_input.lines().count();
+        let cols: usize =raw_seats_input.lines().next().unwrap().chars().count();
         
         for (i, row) in raw_seats_input.lines().enumerate() {
             for (j, seat) in row.chars().enumerate() {
@@ -47,11 +54,17 @@ impl Seats {
             }
         }
 
-        Self {seats}
+        Self {seats, rows, cols}
     }
 
-    fn get_occupied_count(&self) -> usize {
+    fn count_occupied_seats(&self) -> usize {
         self.seats.values().map(|&v| v.is_occupied() as usize).sum()
+    }
+
+    fn equals(&self, new_seats: &HashMap<Seat, Status>) -> bool {
+        self.seats.len() == new_seats.len() && self.seats.iter().all(|(k, v)| {
+            new_seats.get(k).unwrap() == v
+        })
     }
 
     fn allocate_seats_until_stabilization(&mut self) {
@@ -65,14 +78,14 @@ impl Seats {
     fn get_new_seats(&self) -> HashMap<Seat, Status>{
         let mut new_seats = HashMap::<Seat, Status>::new();
         for (&seat, &status) in self.seats.iter() {
-            let new_status = self.apply_seat_rules(seat, status);
+            let new_status = self.apply_seat_rules_1(seat, status);
             new_seats.insert(seat, new_status);
         }
 
         new_seats
     }
 
-    fn apply_seat_rules(&self, seat: Seat, current_status: Status) -> Status {
+    fn apply_seat_rules_1(&self, seat: Seat, current_status: Status) -> Status {
         let adjacent_occupied_count: usize = Seats::adjacent_seats(seat.0 as isize, seat.1 as isize).iter().map(|neighbor| {
             match self.seats.get(&(neighbor.0 as usize, neighbor.1 as usize)) {
                 Some(&status) => match status {
@@ -98,10 +111,72 @@ impl Seats {
         ]
     }
 
-    fn equals(&self, new_seats: &HashMap<Seat, Status>) -> bool {
-        self.seats.len() == new_seats.len() && self.seats.iter().all(|(k, v)| {
-            new_seats.get(k).unwrap() == v
+    fn allocate_seats_until_stabilization_2(&mut self) {
+        let mut new_seats = self.get_new_seats_2();
+        while !self.equals(&new_seats) {
+            self.seats = new_seats;
+            new_seats = self.get_new_seats_2();
+        }
+    }
+
+    fn get_new_seats_2(&self) -> HashMap<Seat, Status>{
+        let mut new_seats = HashMap::<Seat, Status>::new();
+        for (&seat, &status) in self.seats.iter() {
+            let new_status = self.apply_seat_rules_2(seat, status);
+            new_seats.insert(seat, new_status);
+        }
+
+        new_seats
+    }
+
+    fn apply_seat_rules_2(&self, seat: Seat, current_status: Status) -> Status {
+        match self.count_occupied_direction(seat) {
+            0 if current_status != Status::Occupied && current_status != Status::Floor => Status::Occupied,
+            5..=8 if current_status == Status::Occupied => Status::Empty,
+            _ => current_status,
+        }
+    }
+
+    fn count_occupied_direction(&self, seat: Seat) -> usize {
+        [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]
+        .iter()
+        .fold(0, |mut acc, dir| {
+            if self.find_occupied_seats_in_direction(seat, *dir) {
+                acc += 1
+            }
+
+            acc
         })
+    }
+
+    fn find_occupied_seats_in_direction(&self, seat: Seat, direction: (isize, isize)) -> bool {
+        let r_len = self.rows as isize;
+        let c_len = self.cols as isize;
+
+        let (r, c) = ((seat.0 as isize + direction.0) , (seat.1 as isize + direction.1));
+
+        if r < 0 || r >= r_len || c < 0 || c >= c_len {
+            return false;
+        }
+
+        let current_seat = self.seats.get(&(r as usize, c as usize));
+
+        match current_seat {
+            Some(&Status::Empty) => return false,
+            Some(&Status::Occupied) => return true,
+            _ => (),
+        }
+
+        self.find_occupied_seats_in_direction((r as usize, c as usize), direction)
     }
 }
 
@@ -124,7 +199,14 @@ L.LLLLL.LL
         let mut waiting_area = Seats::new(INPUT);
         waiting_area.allocate_seats_until_stabilization();
         println!("current seats: {:?}", waiting_area.seats);
-        assert_eq!(waiting_area.get_occupied_count(), 37);
+        assert_eq!(waiting_area.count_occupied_seats(), 37);
     }
 
+    #[test]
+    fn test_part_two() {
+        let mut waiting_area = Seats::new(INPUT);
+        waiting_area.allocate_seats_until_stabilization_2();
+        println!("current seats: {:?}", waiting_area.seats);
+        assert_eq!(waiting_area.count_occupied_seats(), 26);
+    }
 }
